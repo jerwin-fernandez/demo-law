aed533f93cb996ac360b76a232ca689ac99bc45a
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2020-07-02 16:37:16 +0800

    adding header

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let content1 = document.getElementById('content-1'),
     content2 = document.getElementById('content-2'),
-    content3 = document.getElementById('content-3');
+    content3 = document.getElementById('content-3'),
+    content4 = document.getElementById('content-4');
 
 let btn1 = document.getElementById('btn1'),
     btn2 = document.getElementById('btn2'),
-    btn3 = document.getElementById('btn3');
+    btn3 = document.getElementById('btn3'),
+    btn4 = document.getElementById('btn4');
 
 
 function open1() {
   btn1.style.color = "brown";
   btn2.style.color = "black";
   btn3.style.color = "black";
+  btn4.style.color = "black";
 
 
   // content1.style.transform = "translateX(0)";
@@ -20,12 +23,14 @@ function open1() {
   content1.style.opacity = "1";
   content2.style.opacity = "0";
   content3.style.opacity = "0";
+  content4.style.opacity = "0";
 }
 
 function open2() {
   btn1.style.color = "black";
   btn2.style.color = "brown";
   btn3.style.color = "black";
+  btn4.style.color = "black";
 
   // content1.style.transform = "translateX(100%)";
   // content2.style.transform = "translateX(0)";
@@ -34,6 +39,7 @@ function open2() {
   content1.style.opacity = "0";
   content2.style.opacity = "1";
   content3.style.opacity = "0";
+  content4.style.opacity = "0";
 }
 
 function open3() {
@@ -41,6 +47,7 @@ function open3() {
   btn1.style.color = "black";
   btn2.style.color = "black";
   btn3.style.color = "brown";
+  btn4.style.color = "black";
 
   // content1.style.transform = "translateX(1000%)";
   // content2.style.transform = "translateX(100%)";
@@ -49,5 +56,23 @@ function open3() {
   content1.style.opacity = "0";
   content2.style.opacity = "0";
   content3.style.opacity = "1";
+  content4.style.opacity = "0";
+}
+
+function open4() {
+
+  btn1.style.color = "black";
+  btn2.style.color = "black";
+  btn3.style.color = "black";
+  btn4.style.color = "brown";
+
+  // content1.style.transform = "translateX(1000%)";
+  // content2.style.transform = "translateX(100%)";
+  // content3.style.transform = "translateX(0)";
+
+  content1.style.opacity = "0";
+  content2.style.opacity = "0";
+  content3.style.opacity = "0";
+  content4.style.opacity = "1";
 }
 
